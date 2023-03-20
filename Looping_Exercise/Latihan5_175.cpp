e2218b9ec63e4b709bdf7fe6ee9f3fe24b90caf5
NaufalSatriaa <satriana0017@gmail.com>
2023-03-20 15:44:52 +0700

    i + 1

--- Looping_Exercise/Latihan5_175.cpp
+++ Looping_Exercise/Latihan5_175.cpp
@@ -5,7 +5,7 @@ int main() {
 	int a[5];
 	cout << "mengisi array" << endl;
 	for (int i = 0; i < 5; i++) {
-		cout << "Data ke - " << (i) << ": ";
+		cout << "Data ke - " << (i + 1) << ": ";
 		cin >> a[i];
 	}
 	cout << "mengisi array" << endl;
